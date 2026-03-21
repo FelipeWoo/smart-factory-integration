b1117e1c6007eeef8c2a6a4b4c16cd92caae6ca0
Felipe Woo <felipe.becerra.woo@gmail.com>
2026-03-21 17:22:34 -0600

    basic venv settings

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from utils.boot import boot
 
+
 def main():
     config = boot("main")
-    
+
     # TODO: Add your core logic here
     print(f"Application: {config.name}")
+
 
 if __name__ == "__main__":
     main()
